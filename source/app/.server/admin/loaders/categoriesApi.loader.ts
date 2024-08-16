1d58be298bec9ca71374febaea37df6ea437553a
Daria <helpmeloki7@gmail.com>
2024-08-16 13:16:11 +0300

    add filtrations and sorting

--- source/app/.server/admin/loaders/categoriesApi.loader.ts
+++ source/app/.server/admin/loaders/categoriesApi.loader.ts
@@ -1,9 +1,79 @@
-import { json } from "@remix-run/node";
+import { Prisma } from "@prisma/client";
+import { json, LoaderFunctionArgs } from "@remix-run/node";
 import { prisma } from "~/.server/shared/services/prisma.service"
+import { containsInsensitive } from "~/.server/shared/utils/prisma.util";
+import { ECategoriesSortVariant } from "~/admin/components/categories/Index/Filters";
+import { ESoftDeleteStatus } from "~/admin/constants/entries.constant";
 import { categoryApiMapper } from "../mappers/categoryApi.mapper";
+import {
+  hasNextCalculate,
+  makeQuery,
+  queryToPagination,
+  queryToSearch,
+  queryToSort,
+  requestToSearchParams,
+  sortValueToField
+ } from "../utils/query.util";
+import { categoryQueryValidator } from "./categories/index/loader";
+
+type CategoryOrderByWithRelationInput = Prisma.CategoryOrderByWithRelationInput;
 
 
-export async function categoriesApiLoader(){
+export async function categoriesApiLoader({request}: LoaderFunctionArgs){
+  const searchParams = requestToSearchParams(request);
+  const {data} = await categoryQueryValidator.validate(
+    searchParams
+  );
+  const search = await queryToSearch(searchParams);
+  const pagination = await queryToPagination(searchParams);
+  const sort = await queryToSort(searchParams, ECategoriesSortVariant, ECategoriesSortVariant.createdAt_desc);
+  const orderBy = sortValueToField<CategoryOrderByWithRelationInput>(sort);
+
+  let searchQuery;
+  let filterAccountStatusQuery;
+
+  if (search) {
+    searchQuery = {
+      OR: [
+        {title: containsInsensitive(search)},
+        {slug: containsInsensitive(search)},
+      ]
+    };
+  }
+
+  if (data?.softDeleteStatus === ESoftDeleteStatus.deleted) {
+    filterAccountStatusQuery = {
+      deletedAt: {
+        not: null
+      }
+    };
+  }
+
+  if (data?.softDeleteStatus === ESoftDeleteStatus.active) {
+    filterAccountStatusQuery = {
+      deletedAt: null
+    };
+  }
+
+  const categories = await prisma.category.findMany({
+    take: pagination.take,
+    skip: pagination.skip,
+    where: {
+      ...searchQuery,
+      ...filterAccountStatusQuery,
+    },
+    orderBy
+  });
+
+  pagination.count = categories.length;
+  pagination.total = await prisma.category.count({
+    where: {
+      ...searchQuery,
+      ...filterAccountStatusQuery,
+    }
+  });
+
+  pagination.hasNext = hasNextCalculate(pagination);
 
   const categories = await prisma.category.findMany({
     where: {
@@ -11,5 +81,5 @@ export async function categoriesApiLoader(){
     }
   });
 
-  return json({categories: categories.map(categoryApiMapper)});
+  return json({categories: categories.map(categoryApiMapper), query: makeQuery(search, sort, data), pagination});
 }
